45ffdeafcef43b0c51686ad2368dd7ae76baee65
Prelang Builder <builder@prelang.com>
2015-03-16 15:10:10 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -1,4 +1,8 @@
 class CertificationsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_certification, only: [:show, :edit, :update, :destroy]
 
   # GET /certifications
